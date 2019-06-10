51361cee5d1a731e1267954d84167c80a6536752
Benjamin Clarenc <benjamin.clarenc@gmail.com>
2019-06-10 12:52:38 +0200

    1st commit

---
+++ src/main/java/com/infotel/fiches/FichesDeRenseignementApplication.java
@@ -0,0 +1,13 @@
+package com.infotel.fiches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichesDeRenseignementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichesDeRenseignementApplication.class, args);
+	}
+
+}
