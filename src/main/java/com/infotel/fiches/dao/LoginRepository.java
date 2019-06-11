bd37f5ada745bb193157d0114701a20156f274af
Benjamin Clarenc <benjamin.clarenc@gmail.com>
2019-06-11 14:55:14 +0200

    return de seConnecter

--- src/main/java/com/infotel/fiches/dao/LoginRepository.java
+++ src/main/java/com/infotel/fiches/dao/LoginRepository.java
@@ -1,5 +1,7 @@
 package com.infotel.fiches.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +11,7 @@ import com.infotel.fiches.metier.Login;
 public interface LoginRepository extends JpaRepository<Login, Integer> {
 
 	// (nickname,password) must be on one line of table login
-	@Query("select login from Login where login.nickname=:a INTERSECT select login from Login where login.password=:b")
-	public boolean seConnecter(@Param("a") String nickname, @Param("b") String password);
+	@Query("select login from Login where login.nickname=:a INTERSECT"
+		 + "select login from Login where login.password=:b")
+	public List<Login> seConnecter(@Param("a") String nickname, @Param("b") String password);
 }
