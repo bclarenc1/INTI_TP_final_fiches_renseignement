7ee7712fc56ac52a9db820a0515eaed42f01e6d3
Raph <raphael_bruneau@orange.fr>
2019-06-12 10:14:34 +0200

    Commit Ajout des annotations transactional et modifying

--- src/main/java/com/infotel/fiches/dao/EnfantRepository.java
+++ src/main/java/com/infotel/fiches/dao/EnfantRepository.java
@@ -1,13 +1,17 @@
 package com.infotel.fiches.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.infotel.fiches.metier.Enfant;
 
 public interface EnfantRepository extends JpaRepository<Enfant, Integer> {
 	
+	@Transactional
+	@Modifying(clearAutomatically = true)
 	@Query("update Enfant set idResp=:a where idEnf=:b")
 	public void modifierRespLegal(@Param("b")int idEnf,@Param("a") int idResp);
 
